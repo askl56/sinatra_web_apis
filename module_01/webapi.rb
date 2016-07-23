6a428d9bbe67d50107949fb808fcbde1e9220601
askl56 <amscott@openmailbox.org>
2016-07-23 15:48:30 +0100

    Fix alignment

--- module_01/webapi.rb
+++ module_01/webapi.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'json'
 
 users = {
-  'andrew': { first_name: 'Andrew', last_name: 'Scott', age: 24 },
+  'andrew': 	{ first_name: 'Andrew', last_name: 'Scott', age: 24 },
   'simon':    { first_name: 'Simon', last_name: 'Random', age: 26 },
   'john':     { first_name: 'John', last_name: 'Smith', age: 28 }
 }
