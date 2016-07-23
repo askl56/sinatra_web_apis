c2051cc2f8a349a20301701a5e7bf280fe592d5f
askl56 <amscott@openmailbox.org>
2016-07-23 15:47:40 +0100

    Adds basic users json functionality

--- module_01/webapi.rb
+++ module_01/webapi.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'json'
+
+users = {
+  'andrew': { first_name: 'Andrew', last_name: 'Scott', age: 24 },
+  'simon':    { first_name: 'Simon', last_name: 'Random', age: 26 },
+  'john':     { first_name: 'John', last_name: 'Smith', age: 28 }
+}
 
 get '/' do
 	'master the API'
+end
+
+get '/users' do
+	users.map { |name, data| data.merge(id: name) }.to_json
 end
