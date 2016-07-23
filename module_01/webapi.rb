7f361eea0ece7c261689fef44da64445460407ae
askl56 <amscott@openmailbox.org>
2016-07-23 16:27:05 +0100

    Add media type content negotiation

--- module_01/webapi.rb
+++ module_01/webapi.rb
@@ -3,26 +3,46 @@ require 'json'
 require 'gyoku'
 
 users = {
-  'andrew': 	{ first_name: 'Andrew', last_name: 'Scott', age: 24 },
-  'simon':    { first_name: 'Simon', last_name: 'Random', age: 26 },
-  'john':     { first_name: 'John', last_name: 'Smith', age: 28 }
+  andrew: 	{ first_name: 'Andrew', last_name: 'Scott', age: 24 },
+  simon:    { first_name: 'Simon', last_name: 'Random', age: 26 },
+  john:     { first_name: 'John', last_name: 'Smith', age: 28 }
 }
 
-before do
-  content_type 'application/json'
+helpers do
+
+  def json_or_default?(type)
+    ['application/json', 'application/*', '*/*'].include?(type.to_s)
+  end
+
+  def xml?(type)
+    type.to_s == 'application/xml'
+  end
+
+  def accepted_media_type
+    return 'json' unless request.accept.any?
+
+    request.accept.each do |mt|
+      return 'json' if json_or_default?(mt)
+      return 'xml' if xml?(mt)
+    end
+
+    halt 406, "Not Acceptable"
+  end
+
 end
 
 get '/' do
-  'master the API'
+  'Master Ruby Web APIs - Chapter 2'
 end
 
-['/users', '/users.json'].each do |path|
-  get path do
+get '/users' do
+  type = accepted_media_type
+
+  if type == 'json'
+    content_type 'application/json'
     users.map { |name, data| data.merge(id: name) }.to_json
+  elsif type == 'xml'
+    content_type 'application/xml'
+    Gyoku.xml(users: users)
   end
-end
-
-get '/users.xml' do
-	content_type 'application/xml'
-	Gyoku.xml(users: users)
 end
